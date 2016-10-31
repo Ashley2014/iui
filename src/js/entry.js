76291c23dc848d595bcb446ccd0be2c6920bc444
野猪yz <1746534101@qq.com>
2016-10-31 15:24:36 +0800

    1

---
+++ src/js/entry.js
@@ -0,0 +1,25 @@
+//require("./style.scss");
+
+
+//console.log(url)
+
+var hb={};
+
+
+import interval from "./hb.interval";
+
+
+
+hb.interval=interval;
+
+
+window.hb=hb;
+
+
+//console.log(hb);
+
+
+
+
+
+
